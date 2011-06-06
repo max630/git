d4217b4f886194856d394f21fe65dcaf4bc12e42
Jonathan Nieder <jrnieder@gmail.com>
2011-06-06 04:39:29 -0500

    daemon: move locate_host() to tcp.c
    Keep the different name resolution functions close together so they
    can learn from each other and perhaps share code in the future.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- tcp.h
+++ tcp.h
@@ -1,6 +1,9 @@
 #ifndef TCP_H
 #define TCP_H
 
+extern void git_locate_host(const char *hostname,
+			char **ip_address, char **canon_hostname);
+
 extern int git_use_proxy(const char *host);
 extern void git_tcp_connect(int fd[2], char *host, int flags);
 extern struct child_process *git_proxy_connect(int fd[2], char *host);
