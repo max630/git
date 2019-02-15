8d5af2a8f3ea54a02b104d5feec8bcd0e24bcfec
Max Kirillov <max@max630.net>
2019-02-15 22:18:59 +0200

    Revert "TRY: waitpid-based expect"
    This reverts commit 087ae3b9572711c9e686af13bb31de1537a98464.

--- t/t5562/invoke-with-content-length.pl
+++ t/t5562/invoke-with-content-length.pl
@@ -2,9 +2,6 @@ use 5.008;
 use strict;
 use warnings;
 
-use POSIX ();
-use Time::HiRes qw(usleep);
-
 my $body_filename = $ARGV[0];
 my @command = @ARGV[1 .. $#ARGV];
 
@@ -15,6 +12,11 @@ open(my $body_fh, "<", $body_filename) or die "Cannot open $body_filename: $!";
 my $body_data;
 defined read($body_fh, $body_data, $body_size) or die "Cannot read $body_filename: $!";
 close($body_fh);
+
+my $exited = 0;
+$SIG{"CHLD"} = sub {
+        $exited = 1;
+};
 
 # write data
 my $pid = open(my $out, "|-", @command);
@@ -27,17 +29,7 @@ my $pid = open(my $out, "|-", @command);
 }
 print $out $body_data or die "Cannot write data: $!";
 
-my $counter = 0;
-my $exited = 0;
-while (not $exited and $counter < 6000)
-{
-	usleep 10;
-	if (waitpid($pid, POSIX::WNOHANG) > 0)
-	{
-		$exited = 1;
-	}
-}
-
+sleep 60; # is interrupted by SIGCHLD
 if (!$exited) {
         close($out);
         die "Command did not exit after reading whole body";
